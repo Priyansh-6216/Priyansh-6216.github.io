837d9dad91410aaafa386b211b781216649dbc45
Soni Jay <jaysoni94090@gmail.com>
2026-03-26 15:21:36 -0500

    Fix Vite base path for root deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/priyanshportfolio/',
   plugins: [react()],
   resolve: {
     alias: { '@': '/src' }
